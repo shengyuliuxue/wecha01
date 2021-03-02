ea342cad1e4fa102103028dd15ddfdc3be2ee710
shengyu <594549026@qq.com>
2021-03-02 15:46:52 +0800

    cell code better

--- component/realm/index.js
+++ component/realm/index.js
@@ -1,4 +1,5 @@
 // component/realm/index.js
+import {SkuPending} from "../../model/SkuPending"
 Component({
   /**
    * 组件的属性列表
@@ -11,7 +12,7 @@ Component({
    * 组件的初始数据
    */
   data: {
-     choosenCode: []
+     choosenCode : Object
   },
 
   /**
@@ -25,23 +26,39 @@ Component({
           let status = event.detail.status;
           //console.log(status);
           if(status === "selected"){
-            let code = this.data.choosenCode.concat(event.detail.code);
-            let codeSet = new Set(code)
-            code = Array.from(codeSet)
-            this.setData({
-              choosenCode: code 
-            })          
+            // let code = this.data.choosenCode.concat(event.detail.code);
+            // let codeSet = new Set(code)
+            // code = Array.from(codeSet)
+            // this.setData({
+            //   choosenCode: code 
+            // })    
+            this.data.choosenCode.insertCell(event.detail.code);
+            
           }
           if(status === "waiting"){
-            let code = this.data.choosenCode;
-            let codeSet = new Set(code)
-            codeSet.delete(event.detail.code);
-            code= Array.from(codeSet);
-            this.setData({
-              choosenCode: code
-            })
+            // let code = this.data.choosenCode;
+            // let codeSet = new Set(code)
+            // codeSet.delete(event.detail.code);
+            // code= Array.from(codeSet);
+            // this.setData({
+            //   choosenCode: code
+            // })
+            this.data.choosenCode.deletecell(event.detail.code);
           }
-          console.log(this.data.choosenCode);
+          console.log(this.data.choosenCode.pending);
       }
+  },
+
+  observers : {
+    'data' : function(data){
+      if(!data){
+        return
+      }
+      let skupend = new SkuPending();
+      this.setData({
+        choosenCode : skupend
+      })
+    }
   }
+
 })
--- model/SkuPending.js
+++ model/SkuPending.js
@@ -4,8 +4,17 @@ class SkuPending{
      
   }
 
-  insertCell(){
-    
+  insertCell(code){
+    let incode = this.pending.concat(code)
+    let codeSet = new Set(incode)
+    this.pending = Array.from(codeSet)
+  }
+
+  deletecell(code){
+   let allcode = this.pending;
+   let codeSet = new Set(allcode)
+   codeSet.delete(code);
+   this.pending= Array.from(codeSet);
   }
 }
 
